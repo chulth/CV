c4f4ea834e33ef0cbf451100ddeb53a7e18bd3aa
JohnBulla <chulth@gmail.com>
2020-07-06 22:01:11 -0300

    buld proyect django CV

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+
+def index(request):
+    fecha_actual= 
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+# Create your views here.
